b34ca0986531115ce86b6ed877aa09b1edc31690
jimmytidey <jimmytidey@gmail.com>
2012-02-03 21:14:16 +0000

    iot nearly finished

--- html/script/internet_of_things.js
+++ html/script/internet_of_things.js
@@ -2,7 +2,9 @@
 iot = {};
 
 iot.removeFromReadingPanel = function() {
-
+	
+	iot.transition = true; 
+	
 	if ($('.currently_reading').length != "0") { 
 	
 		$('.currently_reading').animate({'height':"0px"}, { 
@@ -22,7 +24,7 @@ iot.addToIndexPanel = function() {
 	var sinppet_number = $('.currently_reading').attr('id').split("_")[1];
 	$(".currently_reading .big_content").hide();
 	$(".currently_reading .little_content").show();
-	$('#place_holder_' + sinppet_number).html($('.currently_reading'));
+	$('#place_holder_' + sinppet_number).append($('.currently_reading'));
 	$('.currently_reading').css({'width':"230px", "height:":"0px"});	
 	$('.currently_reading').animate({'height':"120px"}, {
 		complete: function() {
@@ -41,12 +43,25 @@ iot.removeFromIndexPanel = function() {
 }
 
 iot.addToReadingPanel = function() { 
-	$('#reading_panel').html(iot.focusSnippet); 
-	$('#reading_panel .snippet').css({'width': '520px', 'height' : '0px'});
+
+	$('#reading_panel').html(iot.focusSnippet);
+	$(iot.focusSnippet).addClass('currently_reading');
+	
+	var sinppet_number = $('.currently_reading').attr('id').split("_")[1];
+	 
+	if (sinppet_number === "1") { 
+		read_height = 1000;
+		
+	}
+
+	else { 
+		read_height = 590;
+	}	
+	
+	$('#reading_panel .snippet').css({'width': '505px', 'height' : '590px'});
 	$(".big_content", iot.focusSnippet).show();
 	$(".little_content", iot.focusSnippet).hide();
-	$('#reading_panel .snippet').animate({'height':"1100px"}); 
-	$(iot.focusSnippet).addClass('currently_reading');
+	iot.transition = false; 
 }
 
 iot.changeFocus = function(elem) { 
@@ -56,30 +71,13 @@ iot.changeFocus = function(elem) {
 
 jQuery(document).ready(function($) {
 	
- 	//init isotope
-	var $container = $('#isotope');
-	iot.container = $('#isotope');
-
-	$container.isotope({
-		itemSelector: '.snippet',
-		masonry : {
-		  columnWidth : 20
-		}
-	});
-	
-	
+ 
 	iot.changeFocus($('#snippet_1').delay(1500));
     
-	// change size of clicked element
-	$container.delegate( '.snippet', 'click', function(){
-	  $(this).toggleClass('large');
-	  $container.isotope('reLayout');
-	});
-
 	// enlarg on click
 	$('.snippet').click(function(){
-		if (parseInt($(this).css('width')) === 230) { 
-			iot.changeFocus(this); 			
+		if (parseInt($(this).css('width')) === 230 && !iot.transition) { 
+			iot.changeFocus(this);		
 		}
 	});
 	
@@ -89,19 +87,6 @@ jQuery(document).ready(function($) {
 		iot.changeFocus(iot.next_snippet_elem);
 	}); 
 
-	//open the first element 
-	$(".intro .big_content").show(); 
-	$(".intro .little_content").hide();
-	$(".intro").css({'width':'520px', 'height':"590px"});
-	$container.isotope('reLayout');	
 
-	//attach scroll down to the arrow 
-	
-	//attach handler to next button 
-	$('.intro_scroll').click(function() {
-
-	   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: 5720}, 500 );
-	   return false;			
-	});
 
 });
