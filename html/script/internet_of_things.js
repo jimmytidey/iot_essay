d5346a5d4a6b22fba625133fae12620308bd9a44
jimmytidey <jimmytidey@gmail.com>
2012-02-06 23:04:20 +0000

    adding proper next buttons

--- html/script/internet_of_things.js
+++ html/script/internet_of_things.js
@@ -57,14 +57,48 @@ iot.addToReadingPanel = function() {
 
 iot.changeFocus = function(elem) {
 	var hash_text = $("h3", elem).html().split('. ')[1].replace(/ /g, "_");
-	window.location.hash = escape(hash_text);
+	hash_text = escape(hash_text);
+	window.location.hash = hash_text;
 	iot.focusSnippet = elem;
 	iot.removeFromReadingPanel();
 }
 
 jQuery(document).ready(function($) {
 	
-	iot.changeFocus($('#snippet_1').delay(600));
+	if (window.location.hash == "#Example_translations_between_information_and_matter") { 
+		iot.changeFocus($('#snippet_1'));
+	}
+
+	else if (window.location.hash == "#") { 
+		window.location.reload();
+	}	
+	
+	else if (window.location.hash == "#Why_it's_important_to_get_the_language_right") {
+		iot.changeFocus($('#snippet_2'));
+	}
+	
+	else if (window.location.hash == "#it's_not_about_\"virtual\"_and_\"real\"") {
+		iot.changeFocus($('#snippet_3'));
+	}
+	
+	else if (window.location.hash == "#\"Material\"_/_\"informational\"_is_a_more_productive_contrast") {
+		iot.changeFocus($('#snippet_4'));
+	}
+	
+	else if (window.location.hash == "#The_continuity_between_\"material\"_and_\"informational\"") {
+		iot.changeFocus($('#snippet_5'));
+	}
+	
+	else if (window.location.hash == "#Revisiting_the_examples") {
+		iot.changeFocus($('#snippet_6'));
+	}
+	
+	else if (window.location.hash == "#Editorial_objects_/_embodied_information") {
+		iot.changeFocus($('#snippet_7'));
+	}					
+
+
+	//iot.changeFocus($('#snippet_1').delay(600));
     
 	// enlarg on click
 	$('.snippet').click(function(){
